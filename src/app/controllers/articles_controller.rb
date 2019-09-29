69e2fcf0525151a70409fd7379e971417202e4b5
Macambira <ruanmacambira@gmail.com>
2019-09-28 22:41:01 -0300

    mudanças nos controllers

--- src/app/controllers/articles_controller.rb
+++ src/app/controllers/articles_controller.rb
@@ -1,4 +1,5 @@
 class ArticlesController < ApplicationController
+  before_action :set_event, except: :index
   before_action :set_article, only: [:show, :edit, :update, :destroy]
 
   # GET /articles
@@ -22,18 +23,13 @@ class ArticlesController < ApplicationController
   end
 
   # POST /articles
-  # POST /articles.json
   def create
     @article = Article.new(article_params)
-
-    respond_to do |format|
-      if @article.save
-        format.html { redirect_to @article, notice: 'Article was successfully created.' }
-        format.json { render :show, status: :created, location: @article }
-      else
-        format.html { render :new }
-        format.json { render json: @article.errors, status: :unprocessable_entity }
-      end
+    @article.user = current_user
+    if @article.save
+      redirect_to [@event, @article], notice: 'Article was successfully created.'
+    else
+      render :new
     end
   end
 
@@ -42,11 +38,9 @@ class ArticlesController < ApplicationController
   def update
     respond_to do |format|
       if @article.update(article_params)
-        format.html { redirect_to @article, notice: 'Article was successfully updated.' }
-        format.json { render :show, status: :ok, location: @article }
+        redirect_to @article, notice: 'Article was successfully updated.'
       else
-        format.html { render :edit }
-        format.json { render json: @article.errors, status: :unprocessable_entity }
+        render :edit
       end
     end
   end
@@ -55,20 +49,25 @@ class ArticlesController < ApplicationController
   # DELETE /articles/1.json
   def destroy
     @article.destroy
-    respond_to do |format|
-      format.html { redirect_to articles_url, notice: 'Article was successfully destroyed.' }
-      format.json { head :no_content }
-    end
+    redirect_to articles_url, notice: 'Article was successfully destroyed.'
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_article
-      @article = Article.find(params[:id])
-    end
+  # Use callbacks to share common setup or constraints between actions.
+  def set_article
+    @article = Article.find(params[:id])
+  end
 
-    # Never trust parameters from the scary internet, only allow the white list through.
-    def article_params
-      params.fetch(:article, {})
-    end
+  # Never trust parameters from the scary internet,
+  # only allow the white list through.
+  def article_params
+    params.require(:article).permit(
+      :title, :abstract, :status, :user_id, :event_id,
+      authors_attributes: %i[name email]
+    )
+  end
+
+  def set_event
+    @event = Event.find(params[:event_id])
+  end
 end
--- src/app/controllers/home_controller.rb
+++ src/app/controllers/home_controller.rb
@@ -2,4 +2,9 @@ class HomeController < ApplicationController
   def index
     @latest_events = Event.latest(5)
   end
+
+  def profile
+    @user = current_user
+    redirect_to root_path if @user.nil?
+  end
 end
