b693922c7db6157255328a2a181a26626db346c0
Macambira <ruanmacambira@gmail.com>
2019-09-28 22:41:50 -0300

    mudanças crud evento

--- src/app/models/event.rb
+++ src/app/models/event.rb
@@ -4,5 +4,26 @@ class Event < ApplicationRecord
   has_and_belongs_to_many :professors
   has_and_belongs_to_many :keywords
 
-  scope :latest, ->(num) { order(:created_at, :desc).limit(num) }
+  scope :latest, ->(num) { order(created_at: :desc).limit(num) }
+
+  def self.search(**params)
+    keywords = Keyword.where(id: params[:keywords])
+    if params
+      where('name LIKE ?', "%#{params[:name]}%")
+        .where('initials LIKE ?', "%#{params[:initials]}%")
+        .where(keywords: keywords)
+    else
+      all
+    end
+  end
+
+  def status
+    if Date.today < submission_start
+      :awaiting
+    elsif (submission_start..submission_finish).include?(Date.today)
+      :opened
+    else
+      :closed
+    end
+  end
 end
