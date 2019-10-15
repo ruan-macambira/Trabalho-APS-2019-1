5e583cdf5a0d56b7976f0b3a4b05bad3f8e72d7f
Macambira <ruanmacambira@gmail.com>
2019-10-14 23:29:55 -0300

    correção comportamento cadastro de eventos
    Campo Professores traziam todos os professores como opção, em vez de
    apenas aqueles que compartilham a mesma área

--- src/app/assets/javascripts/event.js
+++ src/app/assets/javascripts/event.js
@@ -5,7 +5,9 @@ $('#event_keyword_ids').select2();
 
 $('#event_field_id').change(function() {
   $('#event_professor_ids').empty()
-  $.post('/field_professors', function(f) {
+  $.post('/field_professors', {
+      field_id: $('#event_field_id').val()
+    }, function(f) {
     f.forEach(function(e) {
       $('#event_professor_ids').append(
         $(`<option></option>`)
