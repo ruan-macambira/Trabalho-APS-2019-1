ee83eb01ba1d6f420d003b9020aded669a6109bc
Macambira <ruanmacambira@gmail.com>
2019-10-15 23:10:55 -0300

    CRUD palavras-chave

---
+++ src/app/controllers/keywords_controller.rb
@@ -0,0 +1,63 @@
+class KeywordsController < ApplicationController
+  before_action :set_keyword, only: [:show, :edit, :update, :destroy]
+
+  # GET /keywords
+  # GET /keywords.json
+  def index
+    @keywords = Keyword.all
+  end
+
+  # GET /keywords/1
+  # GET /keywords/1.json
+  def show
+  end
+
+  # GET /keywords/new
+  def new
+    @keyword = Keyword.new
+  end
+
+  # GET /keywords/1/edit
+  def edit
+  end
+
+  # POST /keywords
+  # POST /keywords.json
+  def create
+    @keyword = Keyword.new(keyword_params)
+
+    if @keyword.save
+      redirect_to @keyword, notice: 'Keyword was successfully created.'
+    else
+      render :new
+    end
+  end
+
+  # PATCH/PUT /keywords/1
+  # PATCH/PUT /keywords/1.json
+  def update
+    if @keyword.update(keyword_params)
+      redirect_to @keyword, notice: 'Keyword was successfully updated.'
+    else
+      render :edit
+    end
+  end
+
+  # DELETE /keywords/1
+  # DELETE /keywords/1.json
+  def destroy
+    @keyword.destroy
+    redirect_to keywords_url, notice: 'Keyword was successfully destroyed.'
+  end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+  def set_keyword
+    @keyword = Keyword.find(params[:id])
+  end
+
+  # Never trust parameters from the scary internet, only allow the white list through.
+  def keyword_params
+    params.require(:keyword).permit(:word)
+  end
+end
--- src/config/routes.rb
+++ src/config/routes.rb
@@ -8,7 +8,7 @@ Rails.application.routes.draw do
 
   post 'field_professors', to: 'events#field_professors'
 
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :keywords, except: :show
   resources :events do
     resources :articles, except: :index do
       get 'assign', action: 'assign_revisor'
