f6216b0e4b62178a4465095e6be5fd669ea77cd3
Macambira <ruanmacambira@gmail.com>
2019-10-16 23:20:20 -0300

    mudanças js

--- src/app/assets/javascripts/devise.js
+++ src/app/assets/javascripts/devise.js
@@ -1,24 +1,25 @@
-
-$('#user_type').change(function(e) {
-  console.log($('#user_type').val());
-  switch($('#user_type').val()) {
-    case 'Student':
-      $('#form-professor').prop('hidden', true);
-      $('#form-student').prop('hidden', false);
-      $('#user_title').val(null);
-      $('#user_field_id').val(null);
-      break;
-    case 'Professor':
-      $('#form-professor').prop('hidden', false);
-      $('#form-student').prop('hidden', true);
-      $('#user_registration').val(null);
-      break;
-    default:
-      $('#form-professor').prop('hidden', true);
-      $('#form-student').prop('hidden', true);
-      $('#user_title').val(null);
-      $('#user_field_id').val(null);
-      $('#user_registration').val(null);
-      break;
-  }
+$(document).ready(function() {
+  $('#user_type').change(function(e) {
+    console.log($('#user_type').val());
+    switch($('#user_type').val()) {
+      case 'Student':
+        $('#form-professor').prop('hidden', true);
+        $('#form-student').prop('hidden', false);
+        $('#user_title').val(null);
+        $('#user_field_id').val(null);
+        break;
+      case 'Professor':
+        $('#form-professor').prop('hidden', false);
+        $('#form-student').prop('hidden', true);
+        $('#user_registration').val(null);
+        break;
+      default:
+        $('#form-professor').prop('hidden', true);
+        $('#form-student').prop('hidden', true);
+        $('#user_title').val(null);
+        $('#user_field_id').val(null);
+        $('#user_registration').val(null);
+        break;
+    }
+  });
 });
