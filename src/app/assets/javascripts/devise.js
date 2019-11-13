2912c492d984e72b7a8dc7a83eaae2d39c5ba96f
Macambira <ruanmacambira@gmail.com>
2019-11-13 01:56:40 -0300

    novo layout + fix turbolinks

--- src/app/assets/javascripts/devise.js
+++ src/app/assets/javascripts/devise.js
@@ -1,25 +1,29 @@
-$(document).ready(function() {
-  $('#user_type').change(function(e) {
-    console.log($('#user_type').val());
-    switch($('#user_type').val()) {
-      case 'Student':
-        $('#form-professor').prop('hidden', true);
-        $('#form-student').prop('hidden', false);
-        $('#user_title').val(null);
-        $('#user_field_id').val(null);
-        break;
-      case 'Professor':
-        $('#form-professor').prop('hidden', false);
-        $('#form-student').prop('hidden', true);
-        $('#user_registration').val(null);
-        break;
-      default:
-        $('#form-professor').prop('hidden', true);
-        $('#form-student').prop('hidden', true);
-        $('#user_title').val(null);
-        $('#user_field_id').val(null);
-        $('#user_registration').val(null);
-        break;
-    }
-  });
+$(function() {
+  function load() {
+    $('#user_type').change(function(e) {
+      console.log($('#user_type').val());
+      switch($('#user_type').val()) {
+        case 'Student':
+          $('#form-professor').prop('hidden', true);
+          $('#form-student').prop('hidden', false);
+          $('#user_title').val(null);
+          $('#user_field_id').val(null);
+          break;
+        case 'Professor':
+          $('#form-professor').prop('hidden', false);
+          $('#form-student').prop('hidden', true);
+          $('#user_registration').val(null);
+          break;
+        default:
+          $('#form-professor').prop('hidden', true);
+          $('#form-student').prop('hidden', true);
+          $('#user_title').val(null);
+          $('#user_field_id').val(null);
+          $('#user_registration').val(null);
+          break;
+      }
+    });
+  }
+  document.addEventListener("turbolinks:load", load);
+  $(document).ready(load);
 });
--- src/app/assets/javascripts/event.js
+++ src/app/assets/javascripts/event.js
@@ -1,22 +1,24 @@
-$(document).ready(function() {
-  $('#event_field_id').select2();
-  $('#event_professor_ids').select2();
-  $('#event_keyword_ids').select2();
-
-  $('#event_field_id').change(function() {
-    $('#event_professor_ids').empty()
-    $.post('/field_professors', {
-        field_id: $('#event_field_id').val()
-      }, function(f) {
-      f.forEach(function(e) {
-        $('#event_professor_ids').append(
-          $('<option></option>')
-            .val(e.id)
-            .text(e.name)
-        )
+$(function() {
+  function load() {
+    $('.select2').select2();
+  
+    $('#event_field_id').change(function() {
+      $('#event_professor_ids').empty()
+      $.post('/field_professors', {
+          field_id: $('#event_field_id').val()
+        }, function(f) {
+        f.forEach(function(e) {
+          $('#event_professor_ids').append(
+            $('<option></option>')
+              .val(e.id)
+              .text(e.name)
+          )
+        })
       })
     })
-  })
-
-  $('#q_keywords_id_in').select2();
+  
+    $('#q_keywords_id_in').select2();
+  }
+  document.addEventListener("turbolinks:load", load);
+  $(document).ready(load);
 });
