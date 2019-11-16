64a25206f893e936708efe9d88ac202385fbb8cb
Macambira <ruanmacambira@gmail.com>
2019-11-16 14:22:51 -0300

    Mudanças I18n

--- src/app/controllers/application_controller.rb
+++ src/app/controllers/application_controller.rb
@@ -1,6 +1,7 @@
 class ApplicationController < ActionController::Base
   layout :layout_by_resource
   before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :set_title
 
   protected
 
@@ -20,5 +21,13 @@ class ApplicationController < ActionController::Base
     else
       'application'
     end
+  end
+
+  def set_title
+    @title = I18n.t(
+      '.title',
+      scope: [controller_name, action_name],
+      default: I18n.t('default.title')
+    )
   end
 end
